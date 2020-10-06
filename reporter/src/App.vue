873e34cba526318a6a8fa003667813f9419cbcdf
Faye Burke <fburke@miningskills.com.au>
2020-10-06 16:21:22 +1000

    initial styling

--- reporter/src/App.vue
+++ reporter/src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LoginBar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginBar from './components/LoginBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginBar
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
+h6 {
+  font-size: 16px;
+  margin: 0;
+}
+
+button {
+  background-color: white;
+  border: 0;
+  border-radius: 50%;
+  cursor: pointer;
+}
+input:focus {
+    outline: none;
+  }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ reporter/src/components/LoginBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="login-bar">
+    <input type="username" />
+    <input type="password" />
+    <button>
+      <img id="login" src='../assets/icons/enter.svg' />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginBar',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  div.login-bar {
+    width: 500px;
+    height: 100vh;
+    box-shadow: 6px 0 7px -8px grey;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  input {
+    width: 300px;
+    height: 30px;
+    margin: 10px;
+    border: 0;
+    border-bottom: 1px solid grey;
+  }
+
+  #login {
+    margin-top: 30px;
+    width: 50px;
+    font-size: 16px;
+    text-align: center;
+  }
+</style>
